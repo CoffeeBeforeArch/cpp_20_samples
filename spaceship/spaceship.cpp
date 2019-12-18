91daec80bfa78e761a06e156abfe5ac068cef7b7
Nick <CoffeeBeforeArch@gmail.com>
2019-12-17 23:05:53 -0500

    small change

--- spaceship/spaceship.cpp
+++ spaceship/spaceship.cpp
@@ -12,10 +12,10 @@ struct Ints {
   int b;
 
   // The spaceship operator allows us avoid a bunch of boilerplate code!
-  auto operator<=>(const Ints&) const = default;
+  //auto operator<=>(const Ints&) const = default;
 
   // Having to implement all operations like this would be a hassle!
-  /*
+
   bool operator<(const Ints &i) {
     // Compare a's, then b's
     if (a < i.a || (a == i.a && b < i.b)) {
@@ -24,7 +24,7 @@ struct Ints {
       return false;
     }
   }
-  */
+
 };
 
 int main() {
@@ -40,4 +40,6 @@ int main() {
   // And sort them!
   // std::sort() requires the '<' operator!
   std::sort(begin(v), end(v));
+  
+  return 0;
 }
