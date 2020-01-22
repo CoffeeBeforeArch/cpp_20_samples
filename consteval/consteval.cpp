71bd91091b88886f3e2f864161a838822ae1ea71
Nick <CoffeeBeforeArch@gmail.com>
2020-01-21 20:04:48 -0500

    small change

--- consteval/consteval.cpp
+++ consteval/consteval.cpp
@@ -1,25 +1,25 @@
 // This program shows off the basics of consteval in C++20
 // By: Nick from CoffeeBeforeArch
 
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 
 // A consteval function
 // This MUST be evaluated at compile time
 consteval int fibonacci(int N) {
-  if (N <= 1) return 1;
+  if (N <= 1) return N;
   return fibonacci(N - 1) + fibonacci(N - 2);
 }
 
 int main() {
   // This works with our consteval function
   int val = fibonacci(22);
-  
+
   // This does not!
-  // int val2 = fibonacci(rand() % 20);
-  
+  // val = fibonacci(rand() % 20);
+
   // Print the result
-  std::cout << "The fibonacci number is: " << val << '\n'; 
-  
+  std::cout << "The fibonacci number is: " << val << '\n';
+
   return 0;
 }
